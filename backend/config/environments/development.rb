4cf9a21347ebc2268255c995ef4f506f43010778
Faith Adekunle <faith.adekunle.a@gmail.com>
2025-01-17 20:36:29 +0100

    update ports

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 4001 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 7002 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
